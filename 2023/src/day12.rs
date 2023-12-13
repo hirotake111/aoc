4288caf24f4fab4af5b9ab07857717776f9b083b
hirotake111 <hirotake111@users.noreply.github.com>
2023-12-14 05:20:23 +0900

    day12 part2

--- 2023/src/day12.rs
+++ 2023/src/day12.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use std::{num::ParseIntError, time::Instant};
+use std::{collections::HashMap, num::ParseIntError, time::Instant};
 
 #[derive(Debug, PartialEq)]
 struct MyError(String);
@@ -14,9 +14,10 @@ fn part1(input: &str) -> Result<i64, MyError> {
     let record_sets = get_data(input)?;
     let mut total = 0;
     let start = Instant::now();
-    for (i, rs) in record_sets.into_iter().enumerate() {
+    for rs in record_sets {
         // println!("===");
-        let arrangements = get_arrangements(&rs, 0, 0, 0);
+        let mut dp = HashMap::new();
+        let arrangements = get_arrangements(&rs, 0, 0, 0, &mut dp);
         // println!("final arrangements #{i}: {arrangements}");
         total += arrangements;
     }
@@ -24,7 +25,30 @@ fn part1(input: &str) -> Result<i64, MyError> {
     Ok(total)
 }
 
-fn get_arrangements(record_set: &RecordSet, idx: usize, cur_spr: i64, nums_idx: usize) -> i64 {
+fn part2(input: &str) -> Result<i64, MyError> {
+    let record_sets = get_data_v2(input)?;
+    let mut total = 0;
+    let start = Instant::now();
+    for (i, rs) in record_sets.into_iter().enumerate() {
+        let mut dp = HashMap::new();
+        let arrangements = get_arrangements(&rs, 0, 0, 0, &mut dp);
+        println!("final arrangements #{i}: {arrangements}");
+        total += arrangements;
+    }
+    println!(
+        "====\ntime elapsed: {}ms\n====",
+        start.elapsed().as_millis()
+    );
+    Ok(total)
+}
+
+fn get_arrangements(
+    record_set: &RecordSet,
+    idx: usize,
+    cur_spr: i64,
+    nums_idx: usize,
+    dp: &mut HashMap<(usize, i64, usize), i64>,
+) -> i64 {
     if idx == record_set.records.len() {
         // println!("reached the end");
         if nums_idx == record_set.nums.len() && cur_spr == 0 {
@@ -36,20 +60,21 @@ fn get_arrangements(record_set: &RecordSet, idx: usize, cur_spr: i64, nums_idx: 
             return 0;
         }
     }
-    // if record_set.nums[nums_idx] < cur_spr {
-    // return 0;
-    // }
+    if let Some(total) = dp.get(&(idx, cur_spr, nums_idx)) {
+        // println!("cache hit! key: {:?}", (idx, cur_spr, nums_idx));
+        return *total;
+    }
     let mut total = 0;
     match record_set.records[idx] {
         '.' => {
             if cur_spr == 0 {
                 // no sprint to evaluate -> move on to the next
-                total += get_arrangements(record_set, idx + 1, 0, nums_idx);
+                total += get_arrangements(record_set, idx + 1, 0, nums_idx, dp);
             } else {
                 // the end of a spring
                 if nums_idx < record_set.nums.len() && cur_spr == record_set.nums[nums_idx] {
                     // move to the next
-                    total += get_arrangements(record_set, idx + 1, 0, nums_idx + 1);
+                    total += get_arrangements(record_set, idx + 1, 0, nums_idx + 1, dp);
                 } else {
                     // this pattern is invalid -> do nothing
                 }
@@ -58,28 +83,29 @@ fn get_arrangements(record_set: &RecordSet, idx: usize, cur_spr: i64, nums_idx: 
         '#' => {
             if nums_idx < record_set.nums.len() && cur_spr + 1 <= record_set.nums[nums_idx] {
                 // increment cur_spr and move on to the next
-                total += get_arrangements(record_set, idx + 1, cur_spr + 1, nums_idx);
+                total += get_arrangements(record_set, idx + 1, cur_spr + 1, nums_idx, dp);
             } else {
                 // this patterns is invalid -> do nothing
             }
         }
         _ => {
             // #
-            total += get_arrangements(record_set, idx + 1, cur_spr + 1, nums_idx);
+            total += get_arrangements(record_set, idx + 1, cur_spr + 1, nums_idx, dp);
             // .
             if cur_spr == 0 {
                 // no current spring -> just move on to the next
-                total += get_arrangements(record_set, idx + 1, cur_spr, nums_idx);
+                total += get_arrangements(record_set, idx + 1, cur_spr, nums_idx, dp);
             } else {
                 // the end of a spring
                 if nums_idx < record_set.nums.len() && cur_spr == record_set.nums[nums_idx] {
                     // move to the next
-                    total += get_arrangements(record_set, idx + 1, 0, nums_idx + 1);
+                    total += get_arrangements(record_set, idx + 1, 0, nums_idx + 1, dp);
                 }
             }
         }
     }
     // println!("idx: {idx}, total: {total}");
+    dp.insert((idx, cur_spr, nums_idx), total);
     total
 }
 
@@ -92,7 +118,7 @@ fn get_data(input: &str) -> Result<Vec<RecordSet>, MyError> {
             .ok_or(MyError("error parsing record".to_string()))?
             .chars()
             .collect();
-        let nums = line
+        let nums: Vec<i64> = line
             .next()
             .ok_or(MyError("failed parsing num".to_string()))?
             .split(",")
@@ -105,15 +131,57 @@ fn get_data(input: &str) -> Result<Vec<RecordSet>, MyError> {
     Ok(data)
 }
 
+fn get_data_v2(input: &str) -> Result<Vec<RecordSet>, MyError> {
+    let mut data: Vec<RecordSet> = Vec::new();
+    for line in input.lines() {
+        let mut line = line.split_whitespace();
+        let record: Vec<char> = line
+            .next()
+            .ok_or(MyError("error parsing record".to_string()))?
+            .chars()
+            .collect();
+        let nums = line
+            .next()
+            .ok_or(MyError("failed parsing num".to_string()))?
+            .split(",")
+            .map(|c| c.parse::<i64>())
+            .collect::<Result<Vec<i64>, ParseIntError>>()
+            .or_else(|e| Err(MyError(e.to_string())))?;
+
+        let mut records = Vec::with_capacity(record.len() * 5 + 4);
+        for i in 0..5 {
+            for c in &record {
+                records.push(*c);
+            }
+            if i != 4 {
+                records.push('?');
+            }
+        }
+        let l = nums.len();
+        let nums = nums.into_iter().cycle().take(l * 5).collect();
+        println!("{records:?}, {nums:?}");
+        data.push(RecordSet { records, nums });
+    }
+    Ok(data)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_day12() {
+    fn test_day12_part1() {
         let input = std::fs::read_to_string("input/day12_example.txt").unwrap();
         assert_eq!(part1(&input), Ok(21));
         let input = std::fs::read_to_string("input/day12.txt").unwrap();
         assert_eq!(part1(&input), Ok(7344));
+    }
+
+    #[test]
+    fn test_day12_part2() {
+        let input = std::fs::read_to_string("input/day12_example.txt").unwrap();
+        assert_eq!(part2(&input), Ok(525152));
+        let input = std::fs::read_to_string("input/day12.txt").unwrap();
+        assert_eq!(part2(&input), Ok(1088006519007));
     }
 }
