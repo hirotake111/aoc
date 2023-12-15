b7a43a44f31cf4cd9716c93d42b3d241d120920a
hirotake111 <hirotake111@users.noreply.github.com>
2023-12-15 14:09:19 +0900

    day14 part2 (complete)

--- 2023/src/day14.rs
+++ 2023/src/day14.rs
@@ -1,7 +1,8 @@
 #![allow(dead_code)]
 
 fn part1(input: &str) -> usize {
-    let grid = get_data(&input);
+    let mut grid = get_data(&input);
+    tilt_north(&mut grid);
     get_load(&grid)
 }
 
@@ -11,47 +12,25 @@ fn part2(input: &str) -> usize {
     use std::collections::HashMap;
     let original_grid = get_data(&input);
     let mut grid = original_grid.clone();
-    println!("beginning: {}", get_load(&grid));
     let mut grids: Vec<Vec<Vec<char>>> = vec![original_grid.clone()];
     let mut map: HashMap<Vec<Vec<char>>, usize> = HashMap::new();
     map.insert(original_grid.clone(), 0);
 
     for i in 1..=MAX_CYCLE {
-        // println!("~~~~ cycle {i} ~~~~");
         tilt_north(&mut grid);
-        println!("after north: {}", get_load(&grid));
         tilt_west(&mut grid);
-        println!("after west: {}", get_load(&grid));
         tilt_south(&mut grid);
-        println!("after south: {}", get_load(&grid));
         tilt_east(&mut grid);
-        println!("after east: {}", get_load(&grid));
-        // print_grid(&grid);
         grids.push(grid.clone());
-        // grids[cycle] = grid.clone();
         if let Some(&start) = map.get(&grid) {
             // we found the same pattern
-            print_grid(&grid);
             let idx = (MAX_CYCLE - start) % (i - start) + start;
-            println!(
-                "start: {}, i: {} len: {}, idx: {}",
-                start,
-                i,
-                grids.len(),
-                idx
-            );
             let grid = &grids[idx];
             let result = get_load(grid);
-            // let mut clone = grid.clone();
-            // tilt_north(&mut clone);
-            // println!("=== final ===");
-            // print_grid(&clone);
             return result;
         }
-        println!("i: {}, result: {}", i, get_load(&grid));
         map.insert(grid.clone(), i);
     }
-    println!("out of loop");
     get_load(&grids[grids.len() - 1])
 }
 
@@ -73,8 +52,8 @@ fn tilt_north(grid: &mut Vec<Vec<char>>) {
             }
         }
     }
-    // print_grid(grid);
 }
+
 fn tilt_west(grid: &mut Vec<Vec<char>>) {
     let (m, n) = (grid.len(), grid[0].len());
     let mut indexes = vec![0; m];
@@ -93,7 +72,6 @@ fn tilt_west(grid: &mut Vec<Vec<char>>) {
             }
         }
     }
-    // print_grid(grid);
 }
 fn tilt_south(grid: &mut Vec<Vec<char>>) {
     let (m, n) = (grid.len(), grid[0].len());
@@ -117,7 +95,6 @@ fn tilt_south(grid: &mut Vec<Vec<char>>) {
             }
         }
     }
-    // print_grid(grid);
 }
 fn tilt_east(grid: &mut Vec<Vec<char>>) {
     let (m, n) = (grid.len(), grid[0].len());
@@ -141,26 +118,14 @@ fn tilt_east(grid: &mut Vec<Vec<char>>) {
             }
         }
     }
-    // print_grid(grid);
 }
 
 fn get_load(grid: &Vec<Vec<char>>) -> usize {
     let mut total = 0;
-    let (m, n) = (grid.len(), grid[0].len());
-    let mut score = vec![m; n];
-    for i in 0..m {
-        for j in 0..n {
-            match grid[i][j] {
-                'O' => {
-                    total += score[j];
-                    score[j] -= 1;
-                }
-                '#' => {
-                    score[j] = m - i - 1;
-                }
-                _ => {}
-            }
-        }
+    let m = grid.len();
+    for (i, row) in grid.iter().enumerate() {
+        let count = row.iter().filter(|c| **c == 'O').count();
+        total += count * (m - i);
     }
     total
 }
@@ -175,7 +140,6 @@ fn print_grid(grid: &Vec<Vec<char>>) {
         for c in row {
             print!("{c}");
         }
-
         println!("");
     }
     println!("====\n");
@@ -197,7 +161,7 @@ mod tests {
     fn test_part2() {
         let input = std::fs::read_to_string("input/day14_example.txt").unwrap();
         assert_eq!(part2(&input), 64);
-        // let input = std::fs::read_to_string("input/day14.txt").unwrap();
-        // assert_eq!(part2(&input), 108614);
+        let input = std::fs::read_to_string("input/day14.txt").unwrap();
+        assert_eq!(part2(&input), 96447);
     }
 }
